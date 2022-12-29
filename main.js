298dfe9856b40755f311938f3772a55b22931d9f
José Fabrício Figueiredo <81711708+josefabriciofigueiredo@users.noreply.github.com>
2022-12-28 23:23:27 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var valor = location.href.split("?")
+var tam = valor.length-1
+var arquivo = valor[0].split("/")[valor[0].split("/").length-1]
+
+console.log(valor)
+
+if (location.href == "https://perguntasrespostas-e5c00.web.app/") {
+    window.open="https://perguntasrespostas-e5c00.web.app?"
+}
+
+if (valor[tam] == "Resposta01" && arquivo == "perguntasrespostas-e5c00.web.app") {
+    window.location.href = "templates/pergunta02.html?"
+} else if (valor[tam] == "Resposta02" && arquivo == "pergunta02.html") {
+    window.location.href = "pergunta03.html?"
+} else if (valor[tam] == "Resposta03" && arquivo == "pergunta03.html") {
+    window.location.href = "pergunta04.html"
+}
